e8ee31a4b52bac2d3813eee772eee7bd12f3d5cd
unknown <paulhaiduc27@gmail.com>
2020-04-06 14:16:53 +0300

    added all tests for bbt and wbt

--- src/test/java/LabAssiAsseProjectV02/InLabTest.java
+++ src/test/java/LabAssiAsseProjectV02/InLabTest.java
@@ -49,6 +49,137 @@ public class InLabTest {
         assertEquals("Student12", fileRepository1.findOne("12").getNume());
     }
 
+    @Test
+    public void IdTest(){
+        StudentValidator studentValidator = new StudentValidator();
+        StudentXMLRepo fileRepository1 = new StudentXMLRepo("studenti.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        TemaXMLRepo fileRepository2 = new TemaXMLRepo("teme.xml");
+        NotaValidator notaValidator = new NotaValidator(fileRepository1,fileRepository2);
+        NotaXMLRepo fileRepository3 = new NotaXMLRepo("note.xml");
+        Service service = new Service(fileRepository1,studentValidator,fileRepository2,temaValidator,fileRepository3,notaValidator);
+        int count1=0;
+        for (Student s:fileRepository1.findAll())
+            count1++;
+        try {
+            service.addStudent(new Student("", "StudentX", 933, "email1@gmail.com"));
+        }catch (ValidationException msg){
+            System.out.println("Id incorect...");
+        }
+        int count2=0;
+        for (Student s:fileRepository1.findAll())
+            count2++;
+        assertEquals(count1, count2);
+    }
+
+    @Test
+    public void IdNullTest(){
+        StudentValidator studentValidator = new StudentValidator();
+        StudentXMLRepo fileRepository1 = new StudentXMLRepo("studenti.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        TemaXMLRepo fileRepository2 = new TemaXMLRepo("teme.xml");
+        NotaValidator notaValidator = new NotaValidator(fileRepository1,fileRepository2);
+        NotaXMLRepo fileRepository3 = new NotaXMLRepo("note.xml");
+        Service service = new Service(fileRepository1,studentValidator,fileRepository2,temaValidator,fileRepository3,notaValidator);
+        int count1=0;
+        for (Student s:fileRepository1.findAll())
+            count1++;
+        try {
+            service.addStudent(new Student(null, "StudentX", 933, "email1@gmail.com"));
+        }catch (ValidationException msg){
+            System.out.println("Id incorect...");
+        }
+        int count2=0;
+        for (Student s:fileRepository1.findAll())
+            count2++;
+        assertEquals(count1, count2);
+    }
+
+    @Test
+    public void NameTest(){
+        StudentValidator studentValidator = new StudentValidator();
+        StudentXMLRepo fileRepository1 = new StudentXMLRepo("studenti.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        TemaXMLRepo fileRepository2 = new TemaXMLRepo("teme.xml");
+        NotaValidator notaValidator = new NotaValidator(fileRepository1,fileRepository2);
+        NotaXMLRepo fileRepository3 = new NotaXMLRepo("note.xml");
+        Service service = new Service(fileRepository1,studentValidator,fileRepository2,temaValidator,fileRepository3,notaValidator);
+        try {
+            service.addStudent(new Student("13", "", 933, "email1@gmail.com"));
+        }catch (ValidationException msg){
+            System.out.println("Nume incorect...");
+        }
+        assertNull(fileRepository1.findOne("13"));
+    }
+
+    @Test
+    public void NameNullTest(){
+        StudentValidator studentValidator = new StudentValidator();
+        StudentXMLRepo fileRepository1 = new StudentXMLRepo("studenti.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        TemaXMLRepo fileRepository2 = new TemaXMLRepo("teme.xml");
+        NotaValidator notaValidator = new NotaValidator(fileRepository1,fileRepository2);
+        NotaXMLRepo fileRepository3 = new NotaXMLRepo("note.xml");
+        Service service = new Service(fileRepository1,studentValidator,fileRepository2,temaValidator,fileRepository3,notaValidator);
+        try {
+            service.addStudent(new Student("14", null, 933, "email1@gmail.com"));
+        }catch (ValidationException msg){
+            System.out.println("Nume incorect...");
+        }
+        assertNull(fileRepository1.findOne("14"));
+    }
+
+    @Test
+    public void GroupTest(){
+        StudentValidator studentValidator = new StudentValidator();
+        StudentXMLRepo fileRepository1 = new StudentXMLRepo("studenti.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        TemaXMLRepo fileRepository2 = new TemaXMLRepo("teme.xml");
+        NotaValidator notaValidator = new NotaValidator(fileRepository1,fileRepository2);
+        NotaXMLRepo fileRepository3 = new NotaXMLRepo("note.xml");
+        Service service = new Service(fileRepository1,studentValidator,fileRepository2,temaValidator,fileRepository3,notaValidator);
+        try {
+            service.addStudent(new Student("15", "student14", -1, "email1@gmail.com"));
+        }catch (ValidationException msg){
+            System.out.println("Grupa incorect...");
+        }
+        assertNull(fileRepository1.findOne("15"));
+    }
+
+    @Test
+    public void EmailTest(){
+        StudentValidator studentValidator = new StudentValidator();
+        StudentXMLRepo fileRepository1 = new StudentXMLRepo("studenti.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        TemaXMLRepo fileRepository2 = new TemaXMLRepo("teme.xml");
+        NotaValidator notaValidator = new NotaValidator(fileRepository1,fileRepository2);
+        NotaXMLRepo fileRepository3 = new NotaXMLRepo("note.xml");
+        Service service = new Service(fileRepository1,studentValidator,fileRepository2,temaValidator,fileRepository3,notaValidator);
+        try {
+            service.addStudent(new Student("16", "student15", 933, ""));
+        }catch (ValidationException msg){
+            System.out.println("Email incorect...");
+        }
+        assertNull(fileRepository1.findOne("16"));
+    }
+
+    @Test
+    public void EmailNullTest(){
+        StudentValidator studentValidator = new StudentValidator();
+        StudentXMLRepo fileRepository1 = new StudentXMLRepo("studenti.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        TemaXMLRepo fileRepository2 = new TemaXMLRepo("teme.xml");
+        NotaValidator notaValidator = new NotaValidator(fileRepository1,fileRepository2);
+        NotaXMLRepo fileRepository3 = new NotaXMLRepo("note.xml");
+        Service service = new Service(fileRepository1,studentValidator,fileRepository2,temaValidator,fileRepository3,notaValidator);
+        try {
+            service.addStudent(new Student("17", "student15", 933, null));
+        }catch (ValidationException msg){
+            System.out.println("Email incorect...");
+        }
+        assertNull(fileRepository1.findOne("17"));
+    }
+
 
     @Test(expected = ValidationException.class)
     public void ThirdTest(){
@@ -81,6 +212,62 @@ public class InLabTest {
         }catch (ValidationException msg){
             if(msg.getMessage().equals("Deadlineul trebuie sa fie intre 1-14."))
                 throw new ValidationException("Deadlineul trebuie sa fie intre 1-14.");
+        }
+    }
+
+    @Test(expected = ValidationException.class)
+    public void DescriptionTest(){
+        StudentValidator studentValidator = new StudentValidator();
+        StudentXMLRepo fileRepository1 = new StudentXMLRepo("studenti.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        TemaXMLRepo fileRepository2 = new TemaXMLRepo("teme.xml");
+        NotaValidator notaValidator = new NotaValidator(fileRepository1,fileRepository2);
+        NotaXMLRepo fileRepository3 = new NotaXMLRepo("note.xml");
+        Service service = new Service(fileRepository1,studentValidator,fileRepository2,temaValidator,fileRepository3,notaValidator);
+        try {
+            service.addTema(new Tema("1","",1,7));
+        }catch (ValidationException msg){
+            if (msg.getMessage().equals("Descriere invalida!"))
+                throw new ValidationException("Invalid description!");
+        }
+    }
+
+    @Test(expected = ValidationException.class)
+    public void AssignedWeekTest(){
+        StudentValidator studentValidator = new StudentValidator();
+        StudentXMLRepo fileRepository1 = new StudentXMLRepo("studenti.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        TemaXMLRepo fileRepository2 = new TemaXMLRepo("teme.xml");
+        NotaValidator notaValidator = new NotaValidator(fileRepository1,fileRepository2);
+        NotaXMLRepo fileRepository3 = new NotaXMLRepo("note.xml");
+        Service service = new Service(fileRepository1,studentValidator,fileRepository2,temaValidator,fileRepository3,notaValidator);
+        try {
+            service.addTema(new Tema("1","descriere",10,15));
+        }catch (ValidationException msg){
+            if(msg.getMessage().equals("Saptamana primirii trebuie sa fie intre 1-14."))
+                throw new ValidationException("Assigned week must be between 1-14");
+        }
+    }
+
+    @Test
+    public void ValidAssignmentTest(){
+        StudentValidator studentValidator = new StudentValidator();
+        StudentXMLRepo fileRepository1 = new StudentXMLRepo("studenti.xml");
+        TemaValidator temaValidator = new TemaValidator();
+        TemaXMLRepo fileRepository2 = new TemaXMLRepo("teme.xml");
+        NotaValidator notaValidator = new NotaValidator(fileRepository1,fileRepository2);
+        NotaXMLRepo fileRepository3 = new NotaXMLRepo("note.xml");
+        Service service = new Service(fileRepository1,studentValidator,fileRepository2,temaValidator,fileRepository3,notaValidator);
+        try {
+            service.addTema(new Tema("1","descriere1",7,1));
+        }catch (ValidationException msg){
+            throw new ValidationException(msg.getMessage());
+        }
+        int count=0;
+        for (Tema t:fileRepository2.findAll())
+            count++;
+        if (count==1){
+            assertEquals("descriere1", fileRepository2.findOne("1").getDescriere());
         }
     }
 }
