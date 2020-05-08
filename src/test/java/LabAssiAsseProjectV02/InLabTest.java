d5966c84c261010bb74618a0be6aa01f194ff156
unknown <paulhaiduc27@gmail.com>
2020-05-08 18:02:22 +0300

    just one left

--- src/test/java/LabAssiAsseProjectV02/InLabTest.java
+++ src/test/java/LabAssiAsseProjectV02/InLabTest.java
@@ -298,7 +298,7 @@ public class InLabTest {
         NotaXMLRepo fileRepository3 = new NotaXMLRepo("note.xml");
         Service service = new Service(fileRepository1,studentValidator,fileRepository2,temaValidator,fileRepository3,notaValidator);
         service.addStudent(new Student("1", "Student1", 933, "email1@gmail.com"));
-        //assertEquals("Student1", fileRepository1.findOne("1").getNume());
+        assertEquals("Student1", fileRepository1.findOne("1").getNume());
         int countInitial=0;
         for (Tema t:fileRepository2.findAll())
             countInitial++;
@@ -306,7 +306,7 @@ public class InLabTest {
         int count=0;
         for (Tema t:fileRepository2.findAll())
             count++;
-        //assertEquals(count,countInitial+1);
+        assertEquals(count,countInitial+1);
         service.addNota(new Nota("1","1","1",10, LocalDate.now()),"very good");
         //assertEquals("1", fileRepository3.findOne("1").getID());
     }
